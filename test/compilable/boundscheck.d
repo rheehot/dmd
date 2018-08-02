ef3b51f8bd8556a47af6a1c8077fb44072aa6261
Geod24 <pro.mathias.lang@gmail.com>
2018-08-02 17:41:39 +0900

    WIP_boundschecks

--- test/compilable/boundscheck.d
+++ test/compilable/boundscheck.d
@@ -1,8 +1,40 @@
-// REQUIRED_ARGS: -boundscheck=off
+// REQUIRED_ARGS: -boundscheck=safeonly
 
 version (D_BoundsChecks)
     static assert(false)
 
 version (D_NoBoundsChecks) {}
 else
-    static assert(false)
+    static assert(false);
+
+void noBoundsCheck() @safe
+{
+    version (D_BoundsChecks) {}
+    else
+        static assert(false)
+
+    version (D_NoBoundsChecks)
+        static assert(false);
+}
+
+void noBoundsCheck () @system
+{
+    version (D_BoundsChecks)
+        static assert(false)
+
+    version (D_NoBoundsChecks) {}
+    else
+        static assert(false);
+}
+
+/// Must be bounds checked
+void overload (T) (T param)
+{
+    hasBoundsCheck();
+}
+
+/// Not bounds checked
+void overload (T, X = void) (T param)
+{
+    noBoundsCheck();
+}
